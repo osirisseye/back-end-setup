dabe31cefa9ccfc638b6f9bbf9e4b3e8deba0136
Lukasz <lukaszandruchow@gmail.com>
2017-10-01 21:11:41 +0100

    making passport use cookies, polishing authentication

--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,16 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy(
         {
